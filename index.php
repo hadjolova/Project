d3cf68515d95db06ab3cfa70bcd9d631eacd0e7c
hadjolova <mariqhadjolova@gmail.com>
2013-11-29 20:15:00 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+
+<html>
+ <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>DogSite</title>
+        <link rel="stylesheet" type="text/css" href="css/Stylesheet.css" />
+    </head>
+<body>
+<div id="wrapper">
+            <div id="banner">   </div>
+            </div>
+</body>
+	
+	<form action='login.php' method='POST'>
+    Username:<input type='text' name='username'><br>
+    Password: <input type='password' name='password'><br>
+    <input type='submit' value='Login in'>
+    </form>  
+    <a href="register.php"><p style="margin: auto;
+  width:37em;">Register?</a>   
+</html>
